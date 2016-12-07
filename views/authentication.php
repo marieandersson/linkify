1a503e9218fd6ba1e8136bf44f5e4e08ceb466f2
Marie Eriksson <eriksson.km@gmail.com>
2016-12-07 12:42:35 +0100

    create login and register forms.

---
+++ views/authentication.php
@@ -0,0 +1,21 @@
+<div class="forms">
+
+	<div class="loginWrap">
+		<form action="index.php" method="post" class="login">
+			<input type="email" name="email" placeholder="Email">
+			<input type="password" name="password" placeholder="Password">
+			<input type="submit" name="loginSubmit" value="Log in">
+		</form>
+	</div>
+
+	<div class="registerWrap">
+		<form action="index.php" method="post" class="register">
+			<input type="text" name="firstName" placeholder="First name">
+			<input type="text" name="lastName" placeholder="Last name">
+			<input type="email" name="emailReg" placeholder="Email">
+			<input type="password" name="passwordReg" placeholder="Password">
+			<input type="submit" name="registerSubmit" value="Register">
+		</form>
+	</div>
+
+</div>
