b9300640cff88a60ea5aa18269103613f3f4e6f1
Marie Eriksson <eriksson.km@gmail.com>
2017-01-04 13:47:45 +0100

    save votes in local storage

--- assets/scripts/votes.js
+++ assets/scripts/votes.js
@@ -21,37 +21,55 @@ downVoteButtons.forEach (function(downVoteButton) {
 function handleVote(voteButton, vote, number) {
 	let postId = voteButton.parentElement.querySelector(".postIdForVote").value;
 	let postUserId = voteButton.parentElement.querySelector(".postUserIdForVote").value;
+	let loggedInUser = voteButton.parentElement.querySelector(".loggedInUser").value;
 	let errorMessage = document.querySelector(".jsMessage");
-	// put form input in object
-	let postData = new FormData();
-	postData.append("postIdForVote", postId);
-	postData.append("postUserIdForVote", postUserId);
-	postData.append(vote, "vote");
-	// post to php script handling votes
-	fetch("app/posts/votes.php",
-	{
-		method: "POST",
-		body: postData,
-		credentials: "same-origin",
-	})
-	// response after php script has been executed
-	.then(function(response) {
-		// if error
-		if (!response.ok) {
-			return response.text().then(function(error) {
-				errorMessage.innerHTML = error;
-				errorMessage.classList.add("showError");
-			});
-		} else {
-			return response.text().then(function(result) {
-				let votes = oteButton.parentElement.querySelector(".votes");
-				if (result == "vote replaced") {
-					votes.innerHTML = Number(votes.innerHTML) + number + number;
-				}
-				if (result == "new vote saved") {
-					votes.innerHTML = Number(votes.innerHTML) + number;
-				}
-			});
-		}
-	});
+	// check local storage to see if user already voted on post
+	if (localStorage.getItem(loggedInUser+":vote"+postId) == vote) {
+		errorMessage.innerHTML = "You can only vote " + vote + " once.";
+		errorMessage.classList.add("showError");
+	} else if (postUserId == loggedInUser) {
+		errorMessage.innerHTML = "Sorry, you can't vote on you own post.";
+		errorMessage.classList.add("showError");
+	} else {
+		// remove error
+		errorMessage.classList.remove("showError");
+		// put form input in object
+		let postData = new FormData();
+		postData.append("postIdForVote", postId);
+		postData.append("postUserIdForVote", postUserId);
+		postData.append(vote, "vote");
+		// post to php script handling votes
+		fetch("app/posts/votes.php",
+		{
+			method: "POST",
+			body: postData,
+			credentials: "same-origin",
+		})
+		// response after php script has been executed
+		.then(function(response) {
+			// if error
+			if (!response.ok) {
+				return response.text().then(function(error) {
+					errorMessage.innerHTML = error;
+					errorMessage.classList.add("showError");
+				});
+			} else {
+				return response.text().then(function(result) {
+					//remove error
+					errorMessage.classList.remove("showError");
+					let votes = voteButton.parentElement.querySelector(".votes");
+					if (result == "vote replaced") {
+						votes.innerHTML = Number(votes.innerHTML) + number + number;
+						// put vote in local storage
+						localStorage.setItem(loggedInUser+":vote"+postId, vote);
+					}
+					if (result == "new vote saved") {
+						votes.innerHTML = Number(votes.innerHTML) + number;
+						// put vote in local storage
+						localStorage.setItem(loggedInUser+":vote"+postId, vote);
+					}
+				});
+			}
+		});
+	}
 }
