6aba9f57a2be5c6ae0531cfd19ef9b50fc56b86b
Marie Eriksson <eriksson.km@gmail.com>
2017-01-09 22:19:04 +0100

    add profile display to settings page

--- settings.page.php
+++ settings.page.php
@@ -6,67 +6,83 @@ require __DIR__."/views/partials/header.php";
 ?>
 
 	<div class="content">
-		<h2>Account settings</h2>
-
-		<form action="app/settings.php" method="post" class="settingsForm" enctype="multipart/form-data">
-			<?php if ($error) { ?>
-				<div class="settingsError">
-						<?= $error; ?>
-				</div>
-			<?php unset($_SESSION["error"]); } ?>
-			<div class="change">
-				<h3>Change username</h3>
-				<label for="editUsername">Username</label>
-				<input type="text" name="editUsername" value="<?=$user['username']?>">
-			</div>
-
-			<div class="change">
-				<h3>Change email</h3>
-				<label for="editEmail">Email</label>
-				<input type="text" name="editEmail" value="<?=$user['email']?>">
-			</div>
-
-			<div class="change">
-				<h3>Change name</h3>
-				<label for="editFullName">Name</label>
-				<input type="text" name="editFullName" value="<?=$user['name']?>">
-			</div>
-
-
-			<div class="change">
-				<h3>Change password</h3>
-				<label for="editPassword">New password</label>
-				<input type="password" name="editPassword">
-
-				<label for="repeatPassword">Repeat password</label>
-				<input type="password" name="repeatPassword">
-			</div>
-
-			<div class="change">
-				<h3>About</h3>
-					<textarea name="about"><?= $user["about"]?></textarea>
-			</div>
-
-			<div class="change">
-				<h3>Profile picture</h3>
-				<input type="file" name="avatar" accept="image/png, image/jpeg">
-				<div class="placeholderAvatar">
+		<div class="settingsWrap">
+			<div class="displayUserProfile">
+				<figure>
 					<?php if ($user["avatar"] !== NULL) {  ?>
 						<img src="/assets/images/users/<?=$user["id"]?>/<?=$user["avatar"]?>" />
 					<?php } else { ?>
-						<img src="/assets/images/smiley.jpg" />
+						<img src="/assets/images/profileicon.png" />
 					<?php } ?>
-				</div>
+				</figure>
+				<h4><?=$user["name"]?></h4>
+				<h5>@<?=$user["username"]?></h5>
+				<p><?=$user["about"]?></p>
 			</div>
+			<div class="settings">
+				<h2>Account settings</h2>
 
-			<hr/>
+				<form action="app/settings.php" method="post" class="settingsForm" enctype="multipart/form-data">
+					<?php if ($error) { ?>
+						<div class="settingsError">
+								<?= $error; ?>
+						</div>
+					<?php unset($_SESSION["error"]); } ?>
+					<div class="change">
+						<h3>Change username</h3>
+						<label for="editUsername">Username</label>
+						<input type="text" name="editUsername" value="<?=$user['username']?>">
+					</div>
 
-			<div class="change">
-				<label for="saveWithPassword">Password</label><input type="password" name="saveWithPassword">
-				<input type="submit" name="saveChanges" value="Save changes">
+					<div class="change">
+						<h3>Change email</h3>
+						<label for="editEmail">Email</label>
+						<input type="text" name="editEmail" value="<?=$user['email']?>">
+					</div>
+
+					<div class="change">
+						<h3>Change name</h3>
+						<label for="editFullName">Name</label>
+						<input type="text" name="editFullName" value="<?=$user['name']?>">
+					</div>
+
+
+					<div class="change">
+						<h3>Change password</h3>
+						<label for="editPassword">New password</label>
+						<input type="password" name="editPassword">
+
+						<label for="repeatPassword">Repeat password</label>
+						<input type="password" name="repeatPassword">
+					</div>
+
+					<div class="change">
+						<h3>About</h3>
+							<textarea name="about"><?= $user["about"]?></textarea>
+					</div>
+
+					<div class="change">
+						<h3>Profile picture</h3>
+						<input type="file" name="avatar" accept="image/png, image/jpeg">
+						<div class="placeholderAvatar">
+							<?php if ($user["avatar"] !== NULL) {  ?>
+								<img src="/assets/images/users/<?=$user["id"]?>/<?=$user["avatar"]?>" />
+							<?php } else { ?>
+								<img src="/assets/images/smiley.jpg" />
+							<?php } ?>
+						</div>
+					</div>
+
+					<hr/>
+
+					<div class="change">
+						<label for="saveWithPassword">Password</label><input type="password" name="saveWithPassword">
+						<input type="submit" name="saveChanges" value="Save changes">
+					</div>
+
+				</form>
 			</div>
-
-		</form>
+		</div>
 	</div>
 
 </div> <!-- end page -->
