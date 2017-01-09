f50b23a004bfd5807af1971fdc518c5fcdc02716
Marie Eriksson <eriksson.km@gmail.com>
2017-01-09 21:12:23 +0100

    update auth forms css

--- views/partials/navigation.php
+++ views/partials/navigation.php
@@ -1,8 +1,8 @@
 <div class="menuSlide">
 	<p class="menuLink menuClose">X</p>
 	<div class="menu">
+		<a href="/profile.page.php/?user=<?=$_SESSION["login"]["username"]?>">Profile</a>
 		<a href="/settings.page.php">Settings</a>
-		<a href="/profile.page.php/?user=<?=$_SESSION["login"]["username"]?>">My profile</a>
 		<a href="/index.php">Home</a>
 		<a href="/app/auth/logout.php">Log out</a>
 	</div>
