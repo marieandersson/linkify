a15cae45d844e1d6242d035eef94607dd059bde6
Marie Eriksson <eriksson.km@gmail.com>
2017-01-13 11:50:00 +0100

    add footer content

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,3 +1,11 @@
+	<footer>
+		<div class="footerLogo">
+			<img src="/assets/images/logo.png"/>
+			<h5>Linkify</h5>
+		</div>
+		<p>Contact us at contact[at]linkify.com.</p>
+	</footer>
+
 	<script src="/assets/scripts/acceptcookies.js"></script>
 	<script src="/assets/scripts/nav.js"></script>
 	<script src="/assets/scripts/eventlisteners.js"></script>
