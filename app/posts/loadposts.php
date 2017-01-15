c20b559d9fea7d58af2929298cfecd2ea752dcc6
Marie Eriksson <eriksson.km@gmail.com>
2017-01-15 16:13:29 +0100

    identify last post on load more

--- app/posts/loadposts.php
+++ app/posts/loadposts.php
@@ -1,9 +1,23 @@
 <?php
 require __DIR__."/../../autoload.php";
 
-$posts = getPosts($db, $_POST["order"], $_POST["offset"], $_POST["limit"]);
-foreach ($posts as $post) { ?>
+$limit = intval($_POST["limit"]) + 1;
+
+$posts = getPosts($db, $_POST["order"], $_POST["offset"], $limit);
+$numberOfPosts = count($posts);
+$lastPost = true;
+if ($numberOfPosts == $limit) {
+	$lastPost = false;
+}
+
+$i = 0;
+for (; $i < $_POST["limit"] - 1; $i++) {
+  $post = $posts[$i]; ?>
 	<div class="post fadeInPost">
 	<?php include(__DIR__."/../../views/partials/post.block.php"); ?>
 	</div>
-<?php } ?>
+<?php }
+$post = $posts[$i]; ?>
+<div class="post fadeInPost <?= $lastPost ? 'lastPost' : '';?>">
+<?php include(__DIR__."/../../views/partials/post.block.php"); ?>
+</div>
